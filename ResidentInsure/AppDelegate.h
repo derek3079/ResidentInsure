15e147d9fcc9e8ebe9e4a113d043260a0bb20672
derek3079 <derek3079@gmail.com>
2015-04-22 15:00:58 -0600

    Initial Commit

---
+++ ResidentInsure/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ResidentInsure
+//
+//  Created by Derek on 4/22/15.
+//  Copyright (c) 2015 WiredApplications. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ResidentInsure/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ResidentInsure
+//
+//  Created by Derek on 4/22/15.
+//  Copyright (c) 2015 WiredApplications. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
